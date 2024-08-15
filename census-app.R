5f6b2ead1609f71e5d1d06bcd209c8d98596cae9
Taylor AuBuchon-Elder <tauBuchon@mobot.org>
2024-08-15 09:57:40 -0500

    display reactive output and build app

--- census-app.R
+++ census-app.R
@@ -136,8 +136,109 @@ server <- function(input, output) {
 
 shinyApp(ui, server)
 
-install.packages('UScensus2010')
-devtools::install_version('UScensus2010', version = '0.20.0')
 counties <- readRDS("data/counties.rds")
 head(counties)
+
+library(maps)
+library(mapproj)
+
+# helpers.R is an R script that can help you make choropleth maps
+# In our case, helpers.R will create percent_map, a function designed to map the data in counties.rds
+
+source("helpers.R")
+percent_map(counties$white, "darkgreen", "% White")
+# percent_map plots the counties data as a choropleth map
+
+# Take a look at the above code. To use percent_map, we first ran helpers.R with the source function, 
+# and then loaded counties.rds with the readRDS function. We also ran library(maps) and library(mapproj).
+
+# You will need to ask Shiny to call the same functions before it uses percent_map in your app, 
+# but how you write these functions will change. Both source and readRDS require a file path, 
+# and file paths do not behave the same way in a Shiny app as they do at the command line.
+
+# When Shiny runs the commands in server.R, it will treat all file paths as 
+# if they begin in the same directory as server.R. In other words, 
+# the directory that you save server.R in will become the working directory of your Shiny app.
+
+# Since you saved helpers.R in the same directory as server.R, you can ask Shiny to load it with
+source("helpers.R")
+# Since you saved counties.rds in a sub-directory (named data) 
+# of the directory that server.R is in, you can load it with.
+counties <- readRDS("data/counties.rds")
+
+# building the app
+source("helpers.R")
+counties <- readRDS("data/counties.rds")
+library(maps)
+library(mapproj)
+
+# user interface
+
+ui <- page_sidebar(
+  title = "censusVis",
+  
+  sidebar = sidebar(
+    helpText(
+      "Create demographic maps with information from the 2010 US Census."
+    ),
+    selectInput(
+      "var",
+      label = "Choose a variable to display",
+      choices = 
+        c( 
+          "Percent White",
+          "Percent Black",
+          "Percent Hispanic",
+          "Percent Asian"
+          ),
+      selected = "Percent White"
+    ),
+    sliderInput(
+      "range",
+      label = "Range of interest:",
+      min = 0,
+      max = 100,
+      value = c(0,100)
+    )
+  ),
+  card(plotOutput("map"))
+)
+
+# server logic
+# The census visualization app has one reactive object, a plot named "map". 
+# The plot is built with the percent_map function, which takes five arguments.
+server <- function(input, output) {
+  output$map <- renderPlot({
+    data <- switch(input$var,
+                   "Percent White" = counties$white,
+                   "Percent Black" = counties$black,
+                   "Percent Hispanic" = counties$hispanic,
+                   "Percent Asian" = counties$asian)
+    
+    color <- switch(input$var,
+                    "Percent White" = "darkgreen",
+                    "Percent Black" = "black",
+                    "Percent Hispanic" = "darkorange",
+                    "Percent Asian" = "darkviolet")
+    
+    legend <- switch(input$var,
+                     "Percent White" = "% White",
+                     "Percent Black" = "% Black",
+                     "Percent Hispanic" = "% Hispanic",
+                     "Percent Asian" = "% Asian")
+    
+   percent_map(data, color, legend, input$range[1], input$range[2])
+  })
+}
+# switch is a useful companion to multiple choice Shiny widgets. 
+# Use switch to change the values of a widget into R expressions
+
+# run the app
+shinyApp(ui, server)
+
+
+
+
+
+
 
